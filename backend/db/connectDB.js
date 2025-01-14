4fe59944aeaac866e00e49a49adaab4a3b012f30
Sundram Pandey <160123571+SundramPandey29@users.noreply.github.com>
2025-01-14 21:18:04 +0530

    deployment

---
+++ backend/db/connectDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+    try {
+		// Connect to MongoDB database using the provided URI.
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		process.exit(1);
+	}
+};
+
+export default connectDB;
